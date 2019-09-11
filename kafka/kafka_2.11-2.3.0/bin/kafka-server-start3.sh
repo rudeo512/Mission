a983d6992f3b04deb78fcd213779693552b22242
kyungdae.cho <kyungdae.cho@linecorp.com>
2019-09-12 04:36:41 +0900

    kafka jmx 설정 변경

--- kafka/kafka_2.11-2.3.0/bin/kafka-server-start3.sh
+++ kafka/kafka_2.11-2.3.0/bin/kafka-server-start3.sh
@@ -13,33 +13,33 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+export KAFKA_JMX_OPTS='-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Djava.rmi.server.hostname=localhost -Dcom.sun.management.jmxremote.rmi.port=9997'
 export JMX_PORT=9997
 
-if [ $# -lt 1 ];
-then
-	echo "USAGE: $0 [-daemon] server.properties [--override property=value]*"
-	exit 1
+if [ $# -lt 1 ]; then
+  echo "USAGE: $0 [-daemon] server.properties [--override property=value]*"
+  exit 1
 fi
 base_dir=$(dirname $0)
 
 if [ "x$KAFKA_LOG4J_OPTS" = "x" ]; then
-    export KAFKA_LOG4J_OPTS="-Dlog4j.configuration=file:$base_dir/../config/log4j.properties"
+  export KAFKA_LOG4J_OPTS="-Dlog4j.configuration=file:$base_dir/../config/log4j.properties"
 fi
 
 if [ "x$KAFKA_HEAP_OPTS" = "x" ]; then
-    export KAFKA_HEAP_OPTS="-Xmx1G -Xms1G"
+  export KAFKA_HEAP_OPTS="-Xmx1G -Xms1G"
 fi
 
 EXTRA_ARGS=${EXTRA_ARGS-'-name kafkaServer -loggc'}
 
 COMMAND=$1
 case $COMMAND in
-  -daemon)
-    EXTRA_ARGS="-daemon "$EXTRA_ARGS
-    shift
-    ;;
-  *)
-    ;;
+-daemon)
+  EXTRA_ARGS="-daemon "$EXTRA_ARGS
+  shift
+  ;;
+*) ;;
+
 esac
 
 exec $base_dir/kafka-run-class.sh $EXTRA_ARGS kafka.Kafka "$@"
