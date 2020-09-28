758ec31c0ae78ef0a5331703639bebeb7f47b8cd
rudeo0512 <kyungdae.cho@linecorp.com>
2020-09-28 10:13:57 +0900

    es 설정 변경

--- src/main/java/com/rudeo/api/ApiApplication.java
+++ src/main/java/com/rudeo/api/ApiApplication.java
@@ -2,12 +2,14 @@ package com.rudeo.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 @SpringBootApplication
+@EnableElasticsearchRepositories
 public class ApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
 
 }
